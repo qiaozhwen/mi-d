4d73b5727a1df2e9ff24e619483a36b0e3f42005
qiaozhen03 <qiaozhen03@58.com>
2021-11-04 10:19:03 +0800

    Default Changelist

---
+++ eleme-demo/.umirc.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+export default {
+  exportStatic: {
+    htmlSuffix: true,
+  },
+  plugins: [
+    [
+      'umi-plugin-react',
+      {
+        hd: true,
+      },
+    ],
+    [
+      'umi-plugin-datahub',
+      {
+        proxy: {
+          '^/shopping': {
+            hub: 'eleme-demo',
+          },
+          '^/bgs': {
+            hub: 'eleme-demo',
+          },
+        },
+        store: path.join(__dirname, 'data'),
+      },
+    ],
+  ],
+};

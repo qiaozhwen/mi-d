384cb6d721503d824d59c998748813c8af0ddd7d
qiaozhen03 <qiaozhen03@58.com>
2021-11-09 11:01:56 +0800

    add page

--- .umirc.ts
+++ .umirc.ts
@@ -5,7 +5,18 @@ export default defineConfig({
     type: 'none',
   },
   routes: [
-    { path: '/', component: '@/pages/index' },
+    { path: '/', component: '@/pages/index', redirect: '/app' },
+    {
+      path: '/app ',
+      component: '@/pages/index',
+      routes: [
+        { path: '/app/dash-bord', component: '@/pages/index' },
+        { path: '/app/detail', component: '@/pages/index' },
+        { path: '/app/test', component: '@/pages/index' },
+      ],
+    },
+    { path: '/login', component: '@/pages/login' },
+    { path: '/404', component: '@/pages/404' },
   ],
   fastRefresh: {},
 });
---
+++ src/utils/request.js
@@ -0,0 +1,5 @@
+import service from './axiosSetting';
+// const service = axios.create({
+//     baseURL: 'http://localhost:9527/'
+// });
+export default service;
